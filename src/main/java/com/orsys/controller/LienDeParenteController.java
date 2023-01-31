e183fc9e00a302884513e9ac210af977e5c0c167
Vianney-Gln <vgeloen@gmail.com>
2023-01-31 19:25:23 +0100

    création service + controller de récupération des liens de parentés

---
+++ src/main/java/com/orsys/controller/LienDeParenteController.java
@@ -0,0 +1,30 @@
+package com.orsys.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.orsys.business.LienDeParente;
+import com.orsys.services.LienDeParenteServiceImpl;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/")
+@CrossOrigin(origins = "http://localhost:4200")
+public class LienDeParenteController {
+
+	private LienDeParenteServiceImpl lienDeParenteService;
+
+	@GetMapping("lienDeParente")
+	List<LienDeParente> getLienDeParentes() {
+
+		return lienDeParenteService.getLienDeParente();
+
+	}
+
+}
---
+++ src/main/java/com/orsys/services/LienDeParenteServiceImpl.java
@@ -0,0 +1,24 @@
+package com.orsys.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.orsys.business.LienDeParente;
+import com.orsys.dao.ILienDeParenteDao;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class LienDeParenteServiceImpl implements ILienDeParenteService {
+
+	private ILienDeParenteDao lienDeParenteDao;
+
+	@Override
+	public List<LienDeParente> getLienDeParente() {
+
+		return lienDeParenteDao.findAll();
+	}
+
+}
